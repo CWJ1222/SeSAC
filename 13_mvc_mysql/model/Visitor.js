222b3386574c4c93b758c39bde70362c1da7d17e
CWJ1222 <qadzcwsx@gmail.com>
2024-12-02 16:15:52 +0900

    sqlConnection

---
+++ 13_mvc_mysql/model/Visitor.js
@@ -0,0 +1,24 @@
+//DB에 접근하는 ~
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'sesac',
+  password: '1234',
+  database: 'sesac',
+});
+
+exports.getVisitors = (cb) => {
+  //   return [
+  //     { id: 1, name: '홍길동', comment: '내가 왔다.' },
+  //     { id: 2, name: '이찬혁', comment: '으라차차' },
+  //   ];
+  //DB 연결전
+
+  conn.query(`SELECT * FROM visitor `, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    console.log('visitor 테이블의 전체 조회', rows);
+    cb(rows);
+  });
+};
