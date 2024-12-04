d91fe7a44c5dba1bca5252e20313f665d647f2b9
CWJ1222 <qadzcwsx@gmail.com>
2024-12-04 15:18:57 +0900

    mvc_mysql

---
+++ MVC_SOLVE/model/User.js
@@ -0,0 +1,14 @@
+// const realId = "banana";
+// const realPw = "1234";
+
+exports.getUserInfo = () => {
+  return {
+    realId: 'banana',
+    realPw: '1234',
+  };
+};
+
+//실습2풀이
+exports.user = `apple//1234//사과사과
+banana//4321//바나나나나
+cocoa//qwer1234//미떼`;
